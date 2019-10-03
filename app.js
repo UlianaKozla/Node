3adcd8ea284988827d68954ba30d8cb493133b3f
Uliana Kozla <53579961+UlianaKozla@users.noreply.github.com>
2019-10-03 09:56:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 let files = require('./files_creator');
+let info = require('./info_adder');
+let photo = require('./photo_adder');
 
-files('js-18', 'Uliana');
+//створюємо файл студента
+files('js-18', 'Uliana')
 files('js-20', 'Anton');
 
-let info = require('./info_adder');
-
+// створюємо документ студента із екстом що містить його ім'я
 info('js-18', 'Uliana');
 info('js-20', 'Anton');
 
-let photo = require('./photo_adder');
-
+// додаємо фото до папки студента
 photo('js-18', 'Uliana', "1387026468_img0022.png");
