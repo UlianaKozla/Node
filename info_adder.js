f3fde0da5ba4991b57d0523bfcc7089e3bc41822
Uliana Kozla <53579961+UlianaKozla@users.noreply.github.com>
2019-10-03 08:54:23 +0300

    Update info_adder.js

--- info_adder.js
+++ info_adder.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+
 module.exports = (grupaName,studentName)=> {
     fs.writeFile(`./${grupaName}/${studentName}/${studentName}`, `My name is ${studentName}`, {}, err => {
         if (!err) {
