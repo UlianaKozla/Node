5d7f484be1cdc67fe32c9c297f14b958afa27c9b
Uliana Kozla <53579961+UlianaKozla@users.noreply.github.com>
2019-10-03 09:53:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ files('js-18', 'Uliana');
 files('js-20', 'Anton');
 
 let info = require('./info_adder');
+
 info('js-18', 'Uliana');
 info('js-20', 'Anton');
 
 let photo = require('./photo_adder');
+
 photo('js-18', 'Uliana', "1387026468_img0022.png");
